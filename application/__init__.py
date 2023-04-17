2614010a8a0f77e8000725367a74f44d0a3413a4
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-04-17 01:51:11 +0000

    connect backend to mongoDB

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
-
+from flask_pymongo import PyMongo
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "5f5cd6247a3d8ab132a8bcc5bb13c6148102e193"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/gym"
+
+#setup mongoDB
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
+
 
 from application import routes
 
