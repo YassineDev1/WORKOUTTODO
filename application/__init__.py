6d168900e450fa0e3d02ec0af33d264bf2710d1a
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-04-18 17:48:44 +0000

    add bcrypt and jwt to the app

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5f5cd6247a3d8ab132a8bcc5bb13c6148102e193"
 app.config["MONGO_URI"] = "mongodb://localhost:27017/gym"
-
+app.config["JWT_SECRET_KEY"] = "super-secret"
 #setup mongoDB
 mongodb_client = PyMongo(app)
-db = mongodb_client.db
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 
 
