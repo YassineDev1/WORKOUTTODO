50d4d08fda88cd2dc233daeaf186bed80a088ffb
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-04-18 17:52:55 +0000

    create user model with check pass method and find by email

---
+++ application/models.py
@@ -0,0 +1,17 @@
+from application import  mongodb_client
+from flask_bcrypt import generate_password_hash, check_password_hash
+class User:
+    def __init__(self,name, email, password):
+        self.name = name
+        self.email = email
+        self.password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    @staticmethod
+    def find_by_email(email):
+        user = mongodb_client.db.users.find_one({"email": email})
+        if not user:
+            return None
+        return User(user["email"], user["password"])
+
+    
