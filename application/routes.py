0f2206d7cd97a4b2d575956f0a3cca1e9b39e375
Yassine EL MHERZI <elmherziyassine1@gmail.com>
2023-04-18 19:03:52 +0000

    create signup route for users

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from application import app
+from application import app, mongodb_client
 from flask import jsonify, request
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, get_jwt_identity)
 from .models import User
@@ -18,11 +18,40 @@ def login():
     user = User.find_by_email(email)
 
     if not user and not User.check_password(password):
-        return jsonify({"message": "Invalid Email or Password"},401)
+        return jsonify({"message": "Invalid Email or Password"}),401
     access_token = create_access_token(identity=user.email)
     refresh = create_refresh_token(identity=user.email)
 
     return jsonify({
         "access-token": access_token,
         "refresh-toke" : refresh
-    }, 200)
+    }), 200
+
+@app.route("/signup", methods=["POST"])
+
+def register():
+    json = request.json
+    name = json['name']
+    email = json['email']
+    password = json['password']
+
+    if not name :
+        return jsonify({"message": "name is required"})
+    if not email :
+        return jsonify({"message": "email is required"})
+    if not password :
+        return jsonify({"message": "password is required"})
+    
+    existing_user = User.find_by_email(email)
+
+    if not existing_user :
+        user = User(name, email, password)
+        mongodb_client.db.users.insert_one({"name": user.name, "email": user.email, "password": user.password})
+        access_token = create_access_token(identity=user.email)
+        refresh = create_refresh_token(identity=user.email)
+
+        return jsonify({
+            "access-token": access_token,
+            "refresh-token": refresh
+        })
+    return jsonify({"message": "Email Already Exist"})
